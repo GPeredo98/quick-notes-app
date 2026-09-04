d15fa3437b5cb1166002ba5584dfd8787d6aee00
Gabriel Peredo <gperedo@qsmbo.com>
2026-09-03 21:53:25 -0400

    feat: prepare for PWA

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,5 +1,6 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
 import { NOTE_REPOSITORY } from './core/ports/note-repository.port';
 import { RECENT_NOTES_REPOSITORY } from './core/ports/recent-notes-repository.port';
@@ -12,5 +13,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     { provide: NOTE_REPOSITORY, useClass: LocalStorageNoteRepository },
     { provide: RECENT_NOTES_REPOSITORY, useClass: LocalStorageRecentNotesRepository },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
